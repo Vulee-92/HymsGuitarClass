367d6ce4e5ee2728093bf5ee163ce617dfe235eb
Vulee-92 <52331023+Vulee-92@users.noreply.github.com>
2023-08-30 14:47:54 +0700

    update

--- src/components/LoadingComponent/Loading.jsx
+++ src/components/LoadingComponent/Loading.jsx
@@ -1,32 +1,42 @@
-import { Spin } from 'antd'
-import React from 'react'
-import electric from '../../assets/hymnss.gif'
+import { Spin } from 'antd';
+import React, { useState, useEffect } from 'react';
+import electric from '../../assets/hymnss.gif';
 import styles from "./stylemui";
 import Typical from 'react-typical';
+
 const Loading = ({ children, isLoading, deday = 10 }) => {
   const classes = styles();
-  const antIcon = <div style={{
-    fontSize: 50,
-    background: `url(${electric}) no-repeat 50% 50%`,
-    backgroundSize: '5em',
-    pointerEvents: 'none',
-    opacity: 1,
-    position: 'fixed',
-    zIndex: 100000,
-    top: 0,
-    left: 0,
-    width: '100%',
-    height: '100%',
-    animation: 'loaderAnim 1s linear infinite alternate forwards'
-  }}></div>;
+  const [typicalVisible, setTypicalVisible] = useState(true);
+
+  useEffect(() => {
+    if (!isLoading) {
+      setTypicalVisible(false);
+      document.body.classList.remove('loading');
+    } else {
+      document.body.classList.add('loading');
+    }
+  }, [isLoading]);
 
   return (
-    <Spin className={classes.spinner} indicator={antIcon} spinning={isLoading} delay={deday} >
-
-      {children}
-    </Spin>
-
-  )
+    <>
+      {typicalVisible && (
+        <div className={classes.loading}>
+          <div className={classes.overlay}></div>
+          <div className={classes.text}>
+            <Typical
+              steps={['Hymns...', 2000, 'Please wait...', 1000]}
+              loop={Infinity}
+              wrapper="p"
+              delay={1000}
+            />
+          </div>
+        </div>
+      )}
+      <Spin className={classes.spinner} spinning={isLoading} delay={deday}>
+        {children}
+      </Spin>
+    </>
+  );
 }
 
-export default Loading
+export default Loading;
--- src/components/LoadingComponent/stylemui.js
+++ src/components/LoadingComponent/stylemui.js
@@ -6,6 +6,41 @@ import CStyles from "../../utils/common";
 
 export default makeStyles(() => {
   return {
+    text: {
+      fontSize: 50,
+      backgroundSize: "5em",
+      pointerEvents: "none",
+      opacity: 1,
+      zIndex: 100000,
+      textAlign: "center",
+      top: 0,
+      left: 0,
+      display: "flex",
+      width: "100%",
+      alignItems: "center",
+      justifyContent: "center",
+      height: "100%",
+      animation: "loaderAnim 1s linear infinite alternate forwards",
+    },
+    loading: {
+      position: "fixed",
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    overlay: {
+      position: "fixed",
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+      backgroundColor: "rgba(0, 0, 0, 0.1)",
+    },
+
     spinner: {
       animation: "$blink-animation 1s infinite",
     },
