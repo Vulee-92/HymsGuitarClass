1a1262e9fe569ceb3bbd73612b14e409f555571c
Vulee-92 <52331023+Vulee-92@users.noreply.github.com>
2023-12-02 02:48:22 +0700

    b

--- src/components/IconContactAllPageComponent/IconContactAllPageComponent.jsx
+++ src/components/IconContactAllPageComponent/IconContactAllPageComponent.jsx
@@ -1,4 +1,3 @@
-// IconContactAllPageComponent.js
 import React,{ useState,useEffect } from 'react';
 import Popover from '@mui/material/Popover';
 import Box from '@mui/material/Box';
@@ -28,6 +27,7 @@ const OptionBox = styled(Box)({
 const IconContactAllPageComponent = () => {
 	const navigate = useNavigate();
 	const [isPopoverOpen,setPopoverOpen] = useState(false);
+	const [isPopoverOpenSVG,setPopoverOpenSVG] = useState(false);
 	const [isMessengerChatOpen,setMessengerChatOpen] = useState(false);
 
 	const handleIconClick = () => {
@@ -39,6 +39,7 @@ const IconContactAllPageComponent = () => {
 
 		if (option === 'messenger') {
 			setMessengerChatOpen(true);
+			setPopoverOpenSVG(true);
 		}
 	};
 
@@ -47,17 +48,19 @@ const IconContactAllPageComponent = () => {
 	};
 
 	const handleMessengerClose = () => {
-		// Handle the close event of Facebook Messenger
+		// Xử lý sự kiện khi Facebook Messenger được đóng
 		setMessengerChatOpen(false);
+		setPopoverOpenSVG(false);
 	};
 
 	useEffect(() => {
-		// Listen for changes in the DOM to detect the "Đóng" button click
+		// Theo dõi sự thay đổi trong DOM để phát hiện sự kiện nhấp vào nút "Đóng"
 		const observer = new MutationObserver((mutations) => {
 			mutations.forEach((mutation) => {
-				const closeButton = mutation.target.querySelector('.x1o1ewxj');
+				const closeButton = mutation.target.querySelector('.fb_customer_chat_bounce_out_v2');
 				if (closeButton) {
 					closeButton.addEventListener('click',handleMessengerClose);
+					setPopoverOpenSVG(false);
 				}
 			});
 		});
@@ -68,7 +71,7 @@ const IconContactAllPageComponent = () => {
 			observer.observe(fbCustomerChat,{ childList: true,subtree: true });
 
 			return () => {
-				// Cleanup the observer when the component is unmounted
+				// Dọn dẹp observer khi component bị unmount
 				observer.disconnect();
 			};
 		}
@@ -76,28 +79,29 @@ const IconContactAllPageComponent = () => {
 
 	return (
 		<div style={{ position: 'fixed',bottom: '20px',right: '20px',zIndex: '1000' }}>
-			<div
-				style={{
-					width: '44px',
-					height: '44px',
-					backgroundColor: '#245C4F',
-					display: 'flex',
-					justifyContent: 'center',
-					alignItems: 'center',
-					borderRadius: '44px',
-				}}
-			>
-				<svg
-					width="24"
-					height="24"
-					viewBox="0 0 36 36"
-					style={{ cursor: 'pointer',fill: 'white' }}
-					onClick={handleIconClick}
+			{!isPopoverOpenSVG && (
+				<div
+					style={{
+						width: '44px',
+						height: '44px',
+						backgroundColor: '#245C4F',
+						display: 'flex',
+						justifyContent: 'center',
+						alignItems: 'center',
+						borderRadius: '44px',
+					}}
 				>
-					<path d="M34.5 17.2554C34.4986 26.4098 27.5895 33.1113 17.9997 33.1113C16.4018 33.1113 14.8796 32.9689 13.4516 32.6202C12.2642 32.3306 11.0245 32.3602 9.86671 32.755L4.3643 34.4437C3.47806 34.7463 2.61794 33.8851 2.9184 32.9648L4.01091 29.3489C4.42 28.095 4.26117 26.7193 3.57432 25.5988C2.12085 23.2265 1.50069 20.3926 1.5 17.2554V17.254V17.2526C1.50138 8.09894 8.41049 1.50537 17.9997 1.50537C27.5895 1.50537 34.4986 8.09894 34.5 17.2526V17.254V17.2554Z"></path>
-				</svg>
-			</div>
-
+					<svg
+						width="24"
+						height="24"
+						viewBox="0 0 36 36"
+						style={{ cursor: 'pointer',fill: 'white' }}
+						onClick={handleIconClick}
+					>
+						<path d="M34.5 17.2554C34.4986 26.4098 27.5895 33.1113 17.9997 33.1113C16.4018 33.1113 14.8796 32.9689 13.4516 32.6202C12.2642 32.3306 11.0245 32.3602 9.86671 32.755L4.3643 34.4437C3.47806 34.7463 2.61794 33.8851 2.9184 32.9648L4.01091 29.3489C4.42 28.095 4.26117 26.7193 3.57432 25.5988C2.12085 23.2265 1.50069 20.3926 1.5 17.2554V17.254V17.2526C1.50138 8.09894 8.41049 1.50537 17.9997 1.50537C27.5895 1.50537 34.4986 8.09894 34.5 17.2526V17.254V17.2554Z"></path>
+					</svg>
+				</div>
+			)}
 			{isMessengerChatOpen && (
 				<div>
 					<div id="fb-root"></div>
@@ -122,7 +126,7 @@ const IconContactAllPageComponent = () => {
 					<OptionBox>
 						<Typography variant="subtitle1">Gọi cho chúng tôi</Typography>
 						<Typography variant="body2">
-							+84 90 904 36 19 từ 11am đến 7pm để được giải đáp mọi thắc mắc.
+							Gọi +84 90 904 36 19 từ 11am đến 7pm để được giải đáp mọi thắc mắc.
 						</Typography>
 					</OptionBox>
 					<OptionBox style={{ backgroundColor: green[500] }}>
