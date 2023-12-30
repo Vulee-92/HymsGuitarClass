bf8f2f6dad0efbbdeeda608a852be0d88135a3d1
Vulee-92 <52331023+Vulee-92@users.noreply.github.com>
2023-12-30 19:41:46 +0700

    k

--- src/components/ChucTetComponent/ChucTetComponent.jsx
+++ src/components/ChucTetComponent/ChucTetComponent.jsx
@@ -9,7 +9,7 @@ const ChucTetComponent = () => {
 
 	useEffect(() => {
 		const isFirstVisit = localStorage.getItem('isFirstVisit');
-		if (isFirstVisit !== "true") {
+		if (isFirstVisit !== 'true') {
 			setOpen(true);
 			localStorage.setItem('isFirstVisit','false');
 		}
@@ -27,31 +27,39 @@ const ChucTetComponent = () => {
 			aria-describedby="alert-dialog-description"
 			fullWidth
 			maxWidth="sm"
-			style={{ height: '100%',width: '100%' }}
+			style={{ height: '100%',width: '100%',margin: 'auto' }}
 		>
-			<img src={Assets.chuctet} alt="Tết Greetings" style={{ height: '75vh',width: '100%' }} onClick={handleClose} />
+			<img
+				src={Assets.chuctet}
+				alt="Tết Greetings"
+				style={{
+					height: 'auto',
+					width: '100%',
+					maxHeight: '70vh',
+					objectFit: 'fill',
+				}}
+				onClick={handleClose}
+			/>
 			<Button
 				sx={{
 					backgroundColor: `rgb(166, 1, 0)`,
-					color: "rgb(255, 207, 110)",
-					borderTop: "2px solid rgb(255, 207, 110)",
-					fontFamily: "Public Sans",
-					fontSize: "1rem",
-					height: "100%",
-					width: "100%",
+					color: 'rgb(255, 207, 110)',
+					borderTop: '2px solid rgb(255, 207, 110)',
+					fontFamily: 'Public Sans',
+					fontSize: '1rem',
+					height: '100%',
+					width: '100%',
 					fontWeight: 600,
-					cursor: "pointer",
-					"&:hover": {
+					cursor: 'pointer',
+					'&:hover': {
 						backgroundColor: `rgb(255, 207, 110)`,
-						color: "rgb(166, 1, 0)",
-					}
+						color: 'rgb(166, 1, 0)',
+					},
 				}}
 				onClick={handleClose}
 			>
 				Đóng
 			</Button>
-
-
 		</Dialog>
 	);
 };
