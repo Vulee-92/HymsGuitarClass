1d943aa52b0c1d1d3c6309afa5b538dedc6067ff
Vulee-92 <52331023+Vulee-92@users.noreply.github.com>
2023-12-30 18:27:56 +0700

    s

--- src/components/NewYear2024Component/NewYear2024Component.jsx
+++ src/components/NewYear2024Component/NewYear2024Component.jsx
@@ -33,7 +33,7 @@ const NewYear2024Component = () => {
 		};
 	},[]);
 
-	return <div id="fireworks-container" style={{ position: 'absolute',top: 0,left: 0,width: '100%',height: '100%',zIndex: 1000 }} />;
+	return <div id="fireworks-container" style={{ position: 'absolute',top: 0,left: 0,width: '100%',height: '100%',zIndex: 100 }} />;
 };
 
 export default NewYear2024Component;
